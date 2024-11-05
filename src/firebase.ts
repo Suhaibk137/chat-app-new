ca237d50153a5a75ed2a10343e5c34ecde52d3c7
Suhaibk137 <eliteresumehrteam@gmail.com>
2024-11-05 16:29:36 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqCN1JVG8xVcPq6_7Zrn82pg1iqeCsQBI",
-  authDomain: "chat-app-848a6.firebaseapp.com",
-  projectId: "chat-app-848a6",
-  storageBucket: "chat-app-848a6.firebasestorage.app",
-  messagingSenderId: "31591402155",
-  appId: "1:31591402155:web:c131f1eded2eb3959cc786"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com` // Add this line
 };
 
 // Initialize Firebase
